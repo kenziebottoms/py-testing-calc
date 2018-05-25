a23a5419e248292a165f14bfaf08f462094228ef
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-05-25 15:25:56 -0500

    tests running and passing

---
+++ calculator.py
@@ -0,0 +1,19 @@
+class Calculator():
+    """Performs the four basic mathematical operations
+
+    Methods:
+        add(number, number)
+        subtract(number, number)
+        multiply(number, number)
+        divide(number,number)
+    """
+
+    def add(self, x, y):
+        """Adds two numbers together
+
+        Arguments:
+          x - Any number
+          y - An number
+        """
+
+        return x + y
---
+++ calculator.test.py
@@ -0,0 +1,31 @@
+import unittest
+from calculator import Calculator
+
+
+def setUpModule():
+    print('Setting up module...')
+
+
+def tearDownModule():
+    print('Tearing down module...')
+
+
+class TestCalculator(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        print('Setting up class...')
+        self.calc = Calculator()
+
+    @classmethod
+    def tearDownClass(self):
+        print('Tearing down class...')
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 7), 9)
+
+    # Write test methods for subtract, multiply, and divide
+
+
+if __name__ == '__main__':
+    unittest.main()
